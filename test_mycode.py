c439d72c4cf395c39dbf078c169fa6fa675d609a
Haithamak24 <231066@ppu.edu.ps>
2025-05-21 12:35:25 +0300

    fixing the bug on the add negative function and creating a bud on the add positive

--- test_mycode.py
+++ test_mycode.py
@@ -6,7 +6,7 @@ def test_add_positive():
     assert add_numbers(1, 2) == 3
 
 def test_add_zero():
-    assert add_numbers(1, 0) == 1
+    assert add_numbers(1, 0) == 2
 
 def test_add_negative():
-    assert add_numbers(5, -100) == -90
+    assert add_numbers(5, -100) == -95
